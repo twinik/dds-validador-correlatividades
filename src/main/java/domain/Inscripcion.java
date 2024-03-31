6d81bf6daa728258cbcce9ea144f9aec0f3483bb
twinik <tobiwinik@gmail.com>
2024-03-30 22:05:51 -0300

    Initial commit

---
+++ src/main/java/domain/Inscripcion.java
@@ -0,0 +1,19 @@
+package domain;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class Inscripcion {
+    private Alumno alumno;
+    private List<Materia> materias;
+
+    public Inscripcion(Alumno alumno, Materia ... materias) {
+        this.alumno = alumno;
+
+        this.materias = new ArrayList<Materia>();
+        Collections.addAll(this.materias, materias);
+    }
+
+    public boolean aprobada() {
+        return this.materias.stream().allMatch(materia -> this.alumno.puedeCursar(materia));
+    }
+}
---
+++ src/main/java/domain/Materia.java
@@ -0,0 +1,19 @@
+package domain;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Materia {
+    private String nombre;
+    private List<Materia> correlativas;
+
+    public Materia(String nombre, Materia ... correlativas) {
+        this.nombre = nombre;
+        this.correlativas = new ArrayList<Materia>();
+        Collections.addAll(this.correlativas, correlativas);
+    }
+
+    public List<Materia> getCorrelativas() {
+        return correlativas;
+    }
+}
